0a9688a20bf946d5834f16843e9df8f78b393263
Alex <dimension-@live.co.uk>
2017-10-05 21:24:55 +0100

    Reformat JS code to appease JSLint

--- PHP/jscripts/api/faction_details.js
+++ PHP/jscripts/api/faction_details.js
@@ -1,142 +1,138 @@
-$(document).ready(function(){
-    var table_tag = document.getElementById("api-body");
-    var tag_tag = document.getElementById("factiontag");
-    var timestamp_tag = document.getElementById("player_timestamp");
-    
-    if (json_data)
-    {
-      //http://i.imgur.com/nqvSjOB.jpg
-      var timestamp_date = new Date(json_data.Timestamp);
-      timestamp_tag.innerText = timestamp_date.toLocaleString();
-      tag_tag.innerText = new URL(location).searchParams.get("tag");
-      
-      for(var i = 0; i < Object.keys(json_data.Characters).length; i++)
-      {
-          var name = Object.keys(json_data.Characters)[i];
-          var item = json_data.Characters[name];
-      
-          var row_e = document.createElement("tr");
-          row_e.classList.add("api-row");
-          
-          var name_e = document.createElement("td");
-          name_e.innerText = name;
-          row_e.appendChild(name_e);
-          
-          var current_e = document.createElement("td");
-          current_e.innerText = item.Current_Time;
-          row_e.appendChild(current_e);
-          
-          var last_e = document.createElement("td");
-          last_e.innerText = item.Last_Time;
-          row_e.appendChild(last_e);
-                    
-          table_tag.appendChild(row_e);          
-      }
-    }    
+/*global document, location, URL, $, json_data*/
+'use strict';
 
-   var tcats = document.querySelectorAll(".api-headers .tcat");
-    var nameSortTrigger = tcats[nameColNum];
-    var currSortTrigger = tcats[currColNum];
-    var lastSortTrigger = tcats[lastColNum];
-    
-    nameSortTrigger.addEventListener("click", function(e) {
-    	sortTable("name", currentDir);
-    });
-    
-    currSortTrigger.addEventListener("click", function(e) {
-    	sortTable("curr", currentDir);
-    });
-    
-    lastSortTrigger.addEventListener("click", function(e) {
-    	sortTable("last", currentDir);
-    });
-    
-    //Perform the initial sorting by Current Activity
-    sortTable("curr", currentDir);
-    sortTable("curr", currentDir);
-});
-
-/* separate asc/desc sort buttons may be implemented by calling sortTable(sortType, direction) and uncommenting the handler code block */
-var rowSelector = ".api-row";
-
-var nameColNum = 0;
-var currColNum = 1;
-var lastColNum = 2;
-
-
-
-var currentDir = "descending";
-var prevSort = "";
-
-function sortTable(currentSort, dir) {
-	var rowArray = Array.from(document.querySelectorAll(rowSelector));
-	var sortedRowArray = rowArray.sort(function(a, b) {
-		if (currentSort == "last") {
-      var aVal = parseInt(getTimeInt(a.children[lastColNum].innerText));
-			var bVal = parseInt(getTimeInt(b.children[lastColNum].innerText));
-			if (aVal > bVal) return 1;
-			if (aVal < bVal) return -1;
-			return 0;
-		} else if (currentSort == "curr") {
-			var aVal = parseInt(getTimeInt(a.children[currColNum].innerText));
-			var bVal = parseInt(getTimeInt(b.children[currColNum].innerText));
-			if (aVal > bVal) return 1;
-			if (aVal < bVal) return -1;
-			return 0;
-		} else if (currentSort == "name") {
-			var aVal = a.children[nameColNum].innerText.toLowerCase();
-			var bVal = b.children[nameColNum].innerText.toLowerCase();
-			if (aVal > bVal) return 1;
-			if (aVal < bVal) return -1;
-			return 0;
-		}
-	});
-
-	/* sort direction handling for one-button use; remove if using separate asc/desc sort buttons */
-	if (prevSort == currentSort) {
-		if (currentDir == "descending") {
-			sortedRowArray.reverse();
-			currentDir = "ascending";
-		} else {
-			currentDir = "descending";
-		}
-	} else {
-		currentDir = "descending";
-	}
-	prevSort = currentSort;
-	/* end sort direction handler */
-
-	sortedRowArray.forEach(function(row) {
-		document.querySelector(".api-body").insertBefore(row, null);
-	});
-}
+var rowSelector = ".api-row",
+    currentDir = "descending",
+    prevSort = "",
+    nameColNum = 0,
+    currColNum = 1,
+    lastColNum = 2;
 
 function getTimeInt(timeString) {
-  var seconds = 0;
-  var days = 0;
-  
-  var hms = timeString;
-  
-  if (timeString.indexOf("d") != -1) 
-  {    
-		try 
-    {
-			days = parseInt(timeString.match(/([0-9]+)d/)[1]);
-      hms = timeString.substring(timeString.indexOf("d")+1);     
-		} catch (e) 
-    {
-			days = 0;
-		}
-	}
-    
-  var a = hms.split(':');
-  try
-  {
-    seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]); 
-  } catch (e)
-  {
-    seconds = 0;
-  }
-  
-	return (((days * 24) * 60) * 60) + seconds;
+    var seconds = 0,
+        days = 0,
+        hms = timeString,
+        a;
+
+    if (timeString.indexOf("d") !== -1) {
+        try {
+            days = parseInt(timeString.match(/([0-9]+)d/)[1], 10);
+            hms = timeString.substring(timeString.indexOf("d") + 1);
+        } catch (e) {
+            days = 0;
+        }
+    }
+
+    a = hms.split(':');
+    try {
+        seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+    } catch (e) {
+        seconds = 0;
+    }
+
+    return (((days * 24) * 60) * 60) + seconds;
 }
+
+/* separate asc/desc sort buttons may be implemented by calling sortTable(sortType, direction) and uncommenting the handler code block */
+function sortTable(currentSort, dir) {
+    var rowArray = Array.from(document.querySelectorAll(rowSelector)),
+        sortedRowArray = rowArray.sort(function (a, b) {
+            var aVal, bVal;
+            if (currentSort === lastColNum || currentSort === currColNum) {
+                aVal = parseInt(getTimeInt(a.children[currentSort].innerText), 10);
+                bVal = parseInt(getTimeInt(b.children[currentSort].innerText), 10);
+            } else if (currentSort === nameColNum) {
+                aVal = a.children[nameColNum].innerText.toLowerCase();
+                bVal = b.children[nameColNum].innerText.toLowerCase();
+            }
+
+            if (aVal > bVal) {
+                return 1;
+            }
+            if (aVal < bVal) {
+                return -1;
+            }
+            return 0;
+        });
+
+    /* sort direction handling for one-button use; remove if using separate asc/desc sort buttons */
+    if (prevSort === currentSort) {
+        if (dir === "descending") {
+            sortedRowArray.reverse();
+            dir = "ascending";
+        } else {
+            dir = "descending";
+        }
+    } else {
+        dir = "descending";
+    }
+    prevSort = currentSort;
+    /* end sort direction handler */
+
+    sortedRowArray.forEach(function (row) {
+        document.querySelector(".api-body").insertBefore(row, null);
+    });
+}
+
+$(document).ready(function () {
+    var table_tag = document.getElementById("api-body"),
+        tag_tag = document.getElementById("factiontag"),
+        timestamp_tag = document.getElementById("player_timestamp"),
+        tcats = document.querySelectorAll(".api-headers .tcat"),
+        nameSortTrigger = tcats[nameColNum],
+        currSortTrigger = tcats[currColNum],
+        lastSortTrigger = tcats[lastColNum],
+        i,
+        timestamp_date,
+        name,
+        item,
+        row_e,
+        name_e,
+        current_e,
+        last_e;
+
+    if (json_data) {
+        //http://i.imgur.com/nqvSjOB.jpg
+        timestamp_date = new Date(json_data.Timestamp);
+        timestamp_tag.innerText = timestamp_date.toLocaleString();
+        tag_tag.innerText = new URL(location).searchParams.get("tag");
+
+        for (i = 0; i < Object.keys(json_data.Characters).length; i += 1) {
+            name = Object.keys(json_data.Characters)[i];
+            item = json_data.Characters[name];
+
+            row_e = document.createElement("tr");
+            row_e.classList.add("api-row");
+
+            name_e = document.createElement("td");
+            name_e.innerText = name;
+            row_e.appendChild(name_e);
+
+            current_e = document.createElement("td");
+            current_e.innerText = item.Current_Time;
+            row_e.appendChild(current_e);
+
+            last_e = document.createElement("td");
+            last_e.innerText = item.Last_Time;
+            row_e.appendChild(last_e);
+
+            table_tag.appendChild(row_e);
+        }
+    }
+
+    nameSortTrigger.addEventListener("click", function () {
+        sortTable(nameColNum, currentDir);
+    });
+
+    currSortTrigger.addEventListener("click", function () {
+        sortTable(currColNum, currentDir);
+    });
+
+    lastSortTrigger.addEventListener("click", function () {
+        sortTable(lastColNum, currentDir);
+    });
+
+    //Perform the initial sorting by Current Activity
+    sortTable(currColNum, currentDir);
+    sortTable(currColNum, currentDir);
+});
--- PHP/jscripts/api/players_online.js
+++ PHP/jscripts/api/players_online.js
@@ -1,157 +1,155 @@
-$(document).ready(function(){
-    var table_tag = document.getElementById("api-body");
-    var timestamp_tag = document.getElementById("player_timestamp");
-    var count_tag = document.getElementById("player_count");
-    
-    if (json_data)
-    {
-      //http://i.imgur.com/nqvSjOB.jpg
-      var timestamp_date = new Date(json_data.Timestamp);
-      timestamp_tag.innerText = timestamp_date.toLocaleString();
-      count_tag.innerText = json_data.Players.length;
-      
-      for(var i = 0; i < json_data.Players.length; i++)
-      {
-          var item = json_data.Players[i];
-      
-          var row_e = document.createElement("tr");
-          row_e.classList.add("api_player_row");
-          
-          var name_e = document.createElement("td");
-          name_e.innerText = item.Name;
-          row_e.appendChild(name_e);
-          
-          var system_e = document.createElement("td");
-          system_e.innerText = item.System;
-          row_e.appendChild(system_e);
-          
-          var region_e = document.createElement("td");
-          region_e.innerText = item.Region;
-          row_e.appendChild(region_e);
-          
-          var ping_e = document.createElement("td");
-          ping_e.innerText = item.Ping;
-          row_e.appendChild(ping_e);
-          
-          var time_e = document.createElement("td");
-          time_e.innerText = item.Time;
-          row_e.appendChild(time_e);
-          
-          table_tag.appendChild(row_e);          
-      }
-    }    
-    
-    var tcats = document.querySelectorAll(".api-headers .tcat");
-    var nameSortTrigger = tcats[nameColNum];
-    var systemSortTrigger = tcats[sysColNum];
-    var regionSortTrigger = tcats[regionColNum];
-    var pingSortTrigger = tcats[pingColNum];
-    var timeSortTrigger = tcats[timeColNum];
-    
-    nameSortTrigger.addEventListener("click", function(e) {
-    	sortTable("name", currentDir);
-    });
-    
-    systemSortTrigger.addEventListener("click", function(e) {
-    	sortTable("system", currentDir);
-    });
-    
-    regionSortTrigger.addEventListener("click", function(e) {
-    	sortTable("region", currentDir);
-    });
-    
-    pingSortTrigger.addEventListener("click", function(e) {
-    	sortTable("ping", currentDir);
-    });
-    
-    timeSortTrigger.addEventListener("click", function(e) {
-    	sortTable("time", currentDir);
-    });    
-});
+/*global document, $, json_data*/
+'use strict';
 
 /* separate asc/desc sort buttons may be implemented by calling sortTable(sortType, direction) and uncommenting the handler code block */
-var rowSelector = ".api-row";
-
-var nameColNum = 0;
-var sysColNum = 1;
-var regionColNum = 2;
-var pingColNum = 3;
-var timeColNum = 4;
-
-
-
-var currentDir = "descending";
-var prevSort = "";
-
-function sortTable(currentSort, dir) {
-	var rowArray = Array.from(document.querySelectorAll(rowSelector));
-	var sortedRowArray = rowArray.sort(function(a, b) {
-		if (currentSort == "ping") {
-			var aVal = parseInt(a.children[pingColNum].innerText);
-			var bVal = parseInt(b.children[pingColNum].innerText);
-			if (aVal > bVal) return 1;
-			if (aVal < bVal) return -1;
-			return 0;
-		} else if (currentSort == "time") {
-			var aVal = parseInt(getTimeInt(a.children[timeColNum].innerText));
-			var bVal = parseInt(getTimeInt(b.children[timeColNum].innerText));
-			if (aVal > bVal) return 1;
-			if (aVal < bVal) return -1;
-			return 0;
-		} else if (currentSort == "name") {
-			var aVal = a.children[nameColNum].innerText.toLowerCase();
-			var bVal = b.children[nameColNum].innerText.toLowerCase();
-			if (aVal > bVal) return 1;
-			if (aVal < bVal) return -1;
-			return 0;
-		} else if (currentSort == "system") {
-			var aVal = a.children[sysColNum].innerText.toLowerCase();
-			var bVal = b.children[sysColNum].innerText.toLowerCase();
-			if (aVal > bVal) return 1;
-			if (aVal < bVal) return -1;
-			return 0;
-		} else if (currentSort == "region") {
-			var aVal = a.children[regionColNum].innerText.toLowerCase();
-			var bVal = b.children[regionColNum].innerText.toLowerCase();
-			if (aVal > bVal) return 1;
-			if (aVal < bVal) return -1;
-			return 0;
-		}
-	});
-
-	/* sort direction handling for one-button use; remove if using separate asc/desc sort buttons */
-	if (prevSort == currentSort) {
-		if (currentDir == "descending") {
-			sortedRowArray.reverse();
-			currentDir = "ascending";
-		} else {
-			currentDir = "descending";
-		}
-	} else {
-		currentDir = "descending";
-	}
-	prevSort = currentSort;
-	/* end sort direction handler */
-
-	sortedRowArray.forEach(function(row) {
-		document.querySelector(".api-body").insertBefore(row, null);
-	});
-}
+var rowSelector = ".api-row",
+    nameColNum = 0,
+    sysColNum = 1,
+    regionColNum = 2,
+    pingColNum = 3,
+    timeColNum = 4,
+    currentDir = "descending",
+    prevSort = "";
 
 function getTimeInt(timeString) {
-	var minutes = 0;
-	var hours = 0;
-	try {
-		minutes = parseInt(timeString.match(/([0-9]+)(m|\b)/)[1]);
-	} catch (e) {
-		minutes = 0;
-	}
-	if (timeString.indexOf("h") != -1) {
-		try {
-			hours = parseInt(timeString.match(/([0-9]+)h/)[1]);
-		} catch (e) {
-			hours = 0;
-		}
-	}
-	return hours * 60 + minutes;
+    var minutes = 0,
+        hours = 0;
+    try {
+        minutes = parseInt(timeString.match(/([0-9]+)(m|\b)/)[1], 10);
+    } catch (e) {
+        minutes = 0;
+    }
+    if (timeString.indexOf("h") !== -1) {
+        try {
+            hours = parseInt(timeString.match(/([0-9]+)h/)[1], 10);
+        } catch (e) {
+            hours = 0;
+        }
+    }
+    return hours * 60 + minutes;
 }
+
+function sortTable(currentSort, dir) {
+    var rowArray = Array.from(document.querySelectorAll(rowSelector)),
+        sortedRowArray = rowArray.sort(function (a, b) {
+            var aVal, bVal;
+            if (currentSort === "ping") {
+                aVal = parseInt(a.children[pingColNum].innerText, 10);
+                bVal = parseInt(b.children[pingColNum].innerText, 10);
+            } else if (currentSort === "time") {
+                aVal = parseInt(getTimeInt(a.children[timeColNum].innerText), 10);
+                bVal = parseInt(getTimeInt(b.children[timeColNum].innerText), 10);
+            } else if (currentSort === "name") {
+                aVal = a.children[nameColNum].innerText.toLowerCase();
+                bVal = b.children[nameColNum].innerText.toLowerCase();
+            } else if (currentSort === "system") {
+                aVal = a.children[sysColNum].innerText.toLowerCase();
+                bVal = b.children[sysColNum].innerText.toLowerCase();
+            } else if (currentSort === "region") {
+                aVal = a.children[regionColNum].innerText.toLowerCase();
+                bVal = b.children[regionColNum].innerText.toLowerCase();
+            }
+            if (aVal > bVal) {
+                return 1;
+            }
+            if (aVal < bVal) {
+                return -1;
+            }
+            return 0;
+        });
+
+    /* sort direction handling for one-button use; remove if using separate asc/desc sort buttons */
+    if (prevSort === currentSort) {
+        if (dir === "descending") {
+            sortedRowArray.reverse();
+            dir = "ascending";
+        } else {
+            dir = "descending";
+        }
+    } else {
+        dir = "descending";
+    }
+    prevSort = currentSort;
+    /* end sort direction handler */
+
+    sortedRowArray.forEach(function (row) {
+        document.querySelector(".api-body").insertBefore(row, null);
+    });
+}
+
+$(document).ready(function () {
+    var table_tag = document.getElementById("api-body"),
+        timestamp_tag = document.getElementById("player_timestamp"),
+        count_tag = document.getElementById("player_count"),
+        tcats = document.querySelectorAll(".api-headers .tcat"),
+        nameSortTrigger = tcats[nameColNum],
+        systemSortTrigger = tcats[sysColNum],
+        regionSortTrigger = tcats[regionColNum],
+        pingSortTrigger = tcats[pingColNum],
+        timeSortTrigger = tcats[timeColNum],
+        i,
+        timestamp_date,
+        item,
+        row_e,
+        name_e,
+        system_e,
+        region_e,
+        ping_e,
+        time_e;
+
+    if (json_data) {
+        //http://i.imgur.com/nqvSjOB.jpg
+        timestamp_date = new Date(json_data.Timestamp);
+        timestamp_tag.innerText = timestamp_date.toLocaleString();
+        count_tag.innerText = json_data.Players.length;
+
+        for (i = 0; i < json_data.Players.length; i += 1) {
+            item = json_data.Players[i];
+
+            row_e = document.createElement("tr");
+            row_e.classList.add("api_player_row");
+
+            name_e = document.createElement("td");
+            name_e.innerText = item.Name;
+            row_e.appendChild(name_e);
+
+            system_e = document.createElement("td");
+            system_e.innerText = item.System;
+            row_e.appendChild(system_e);
+
+            region_e = document.createElement("td");
+            region_e.innerText = item.Region;
+            row_e.appendChild(region_e);
+
+            ping_e = document.createElement("td");
+            ping_e.innerText = item.Ping;
+            row_e.appendChild(ping_e);
+
+            time_e = document.createElement("td");
+            time_e.innerText = item.Time;
+            row_e.appendChild(time_e);
+
+            table_tag.appendChild(row_e);
+        }
+    }
+
+    nameSortTrigger.addEventListener("click", function () {
+        sortTable("name", currentDir);
+    });
+
+    systemSortTrigger.addEventListener("click", function () {
+        sortTable("system", currentDir);
+    });
+
+    regionSortTrigger.addEventListener("click", function () {
+        sortTable("region", currentDir);
+    });
+
+    pingSortTrigger.addEventListener("click", function () {
+        sortTable("ping", currentDir);
+    });
+
+    timeSortTrigger.addEventListener("click", function () {
+        sortTable("time", currentDir);
+    });
+});
